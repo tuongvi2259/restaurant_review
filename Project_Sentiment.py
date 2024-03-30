85dee5acab207a147bd19936da97794a74fac673
tuongvi2259 <165400340+tuongvi2259@users.noreply.github.com>
2024-03-30 10:30:24 +0700

    Update Project_Sentiment.py

--- Project_Sentiment.py
+++ Project_Sentiment.py
@@ -212,7 +212,7 @@ elif choice == 'Setiment Analysis':
         uploaded_file = st.file_uploader("Chọn file dữ liệu", type=["csv"])
         if uploaded_file is not None:
             # Đọc file dữ liệu
-            file_contents = uploaded_file.getvalue().decode("utf-16")
+            file_contents = uploaded_file.getvalue().decode("latin-1")
             # Kiểm tra xem dấu ngăn cách là dấu phẩy hay dấu chấm phẩy
             if ";" in file_contents:
                 # Thay thế dấu chấm phẩy bằng dấu phẩy
